d99037c9725a23bf6423b6a5ac3e787e9e618570
Vishal Rathod <vishalrathod191918@gmail.com>
2025-11-18 21:10:45 +0530

    feat: constructor

---
+++ Student.h
@@ -0,0 +1,25 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student {
+    public:
+    int roll;
+    string name;
+    string course;
+
+    Student() : roll(0) , name(""), course(""){}
+
+    Student(int r, const string &n , const string &c) :  roll(r), name(n), course(c){}
+
+    void display() const {
+        cout<<"Roll: "<<roll
+            <<" | Name: "<<name 
+            <<" | Course: "<<course<<' \n';
+    };
+
+};
+#endif
